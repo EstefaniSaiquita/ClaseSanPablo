4a3f022fbdc2848f551095e1185e5afb19baace4
EstefaniSaiquita <naomi.tefi@gmail.com>
2024-04-17 06:51:18 -0300

    numero randoms

--- clase2/encontrar.php
+++ clase2/encontrar.php
@@ -3,5 +3,10 @@ $nro = [];
 for ($i=1; $i <= 50 ; $i++) { 
   $nro[$i] = Rand(0, 100);
 echo "<br>";
-echo "$nro[$i]";
+echo "$nro[$i]\n";
+if ($nro[$i]== 5) {
+   echo "se encontro el numero en la posicion $i ";
+} else {
+   echo "no se encontro el numero";
+} 
 };
