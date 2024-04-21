76ce426a4ce43ad5244893b3e148c42c25140023
EstefaniSaiquita <naomi.tefi@gmail.com>
2024-04-21 05:06:57 -0300

    otra vez el ejercicio pares

---
+++ clase2/encontrar.php
@@ -0,0 +1,12 @@
+<?php
+$nro = [];
+for ($i=1; $i <= 50 ; $i++) { 
+  $nro[$i] = Rand(0, 100);
+echo "<br>";
+echo "$nro[$i]\n";
+if ($nro[$i]== 5) {
+   echo "se encontro el numgro en la posicion $i ";
+} else {
+   echo "no se encontro el numero";
+} 
+};
