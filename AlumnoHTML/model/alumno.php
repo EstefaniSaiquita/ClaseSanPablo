6f2670da196da12e05407fa543e622a0f8f91a70
EstefaniSaiquita <naomi.tefi@gmail.com>
2024-08-08 20:48:15 -0300

    Creacion de models, views y controllers

---
+++ AlumnoHTML/model/alumno.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'Conexion.php';
+
+class Alumno extends Conexion {
+
+    public $id, $nombre, $apellido, $fecha_nacimiento;
+
+    public function create() {
+        $this->conectar();
+        $pre = mysqli_prepare($this->con, "INSERT INTO alumnos (nombre, apellido, fecha_nacimiento) VALUES (?, ?, ?)");
+        $pre->bind_param("sss", $this->nombre, $this->apellido, $this->fecha_nacimiento);
+        $pre->execute();
+    }
+
+}
---
+++ AlumnoHTML/views/createAlumno.view.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<h1>Crear alumno</h1>
+
+<form action="alumno.php" method="post">
+    <label for="nombre">Nombre</label>
+    <input type="text" name="nombre" id="nombre">
+</form>
+</body>
+</html>
